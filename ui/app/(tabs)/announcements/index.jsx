3d682e5789528a9228fb4fb37ed9d52431a8a89b
miles-evan <miles.todtfeld@gmail.com>
2025-08-12 15:39:45 -0400

    made it so only supervisors see the message input

--- ui/app/(tabs)/announcements/index.jsx
+++ ui/app/(tabs)/announcements/index.jsx
@@ -10,12 +10,15 @@ import { KeyboardAvoidingView, Platform, View } from "react-native";
 import LinearAnimatedView from "@/components/general-utility-components/LinearAnimatedView.jsx";
 import SendMessageButton from "@/components/announcements-and-messaging/SendMessageButton.jsx";
 import Gap from "@/components/general-utility-components/Gap.jsx";
+import { useGlobalState } from "@/hooks/useGlobalState.js";
+import If from "@/components/general-utility-components/If.jsx";
 
 export default function Announcements() {
 	const isFocused = useIsFocused();
 	const sendMessageRef = useRef(null);
 	const valueRef = useRef(null);
 	const setValueRef = useRef(null);
+	const globalState = useGlobalState();
 	
 	function sendMessage() {
 		sendMessageRef.current(valueRef.current);
@@ -34,13 +37,15 @@ export default function Announcements() {
 					<AnnouncementsList isFocused={isFocused} sendMessageRef={sendMessageRef}/>
 				</View>
 				
-				<LinearAnimatedView>
-					<FlexRowSpaceBetween style={{ paddingBottom: 8 }}>
-						<MessageInput valueRef={valueRef} setValueRef={setValueRef}/>
-						<Gap horizontal size={10}/>
-						<SendMessageButton onPress={sendMessage}>Send</SendMessageButton>
-					</FlexRowSpaceBetween>
-				</LinearAnimatedView>
+				<If condition={globalState.isSupervisor}>
+					<LinearAnimatedView>
+						<FlexRowSpaceBetween style={{ paddingBottom: 8 }}>
+							<MessageInput valueRef={valueRef} setValueRef={setValueRef}/>
+							<Gap horizontal size={10}/>
+							<SendMessageButton onPress={sendMessage}>Send</SendMessageButton>
+						</FlexRowSpaceBetween>
+					</LinearAnimatedView>
+				</If>
 			</KeyboardAvoidingView>
 		</SafeAreaViewWithBackground>
 	);
