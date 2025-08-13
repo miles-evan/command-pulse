093709ce7d11015fe1d636d167f4745214dbb93d
miles-evan <miles.todtfeld@gmail.com>
2025-08-13 15:04:54 -0400

    implemented accepting shift requests

--- ui/components/announcements-and-messaging/ShiftRequestCard.jsx
+++ ui/components/announcements-and-messaging/ShiftRequestCard.jsx
@@ -4,26 +4,38 @@ import Contact from "@/components/contacts/Contact.jsx";
 import If from "@/components/general-utility-components/If.jsx";
 import StyledText from "@/components/general-utility-components/StyledText.jsx";
 import HorizontalLine from "@/components/general-utility-components/HorizontalLine.jsx";
+import { useGlobalState } from "@/hooks/useGlobalState.js";
+import * as shiftService from "@/services/shiftService.js";
+import { useState } from "react";
 
 
 export default function ShiftRequestCard({ shiftRequest }) {
 	
 	const { shiftRequestId, shift, message, isCover, timeSent } = shiftRequest;
+	const { userId } = useGlobalState();
+	const [loadingAccept, setLoadingAccept] = useState(false);
+	
+	
+	async function accept() {
+		setLoadingAccept(true);
+		await shiftService.acceptShiftRequest(shiftRequestId);
+		setLoadingAccept(false);
+	}
+	
 	
 	return (
 		<ShiftCard
 			shift={shift}
-			HeaderComponent={isCover && (
+			HeaderComponent={isCover && userId !== shift.userId (
 				<Contact user={{ firstName: shift.firstName, lastName: shift.lastName }}/>
 			)}
 			FooterComponent={
 				<>
 					<HorizontalLine/>
-					<StyledText look="24 regular veryHard">{`${isCover? "Cover" : "Shift"} Request`}</StyledText>
 					<If condition={message}>
-						<StyledText look="18 regular veryHard">{message}</StyledText>
+						<StyledText look="24 regular veryHard">{message}</StyledText>
 					</If>
-					<Button>Accept</Button>
+					<Button onPress={accept} disabled={loadingAccept}>Accept</Button>
 				</>
 			}
 		/>
