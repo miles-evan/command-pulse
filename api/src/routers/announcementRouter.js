c15c29c0de76f792f5f55bbdb09272ac89e93128
miles-evan <miles.todtfeld@gmail.com>
2025-08-14 14:00:12 -0400

    added validation for announcements endpoints

--- api/src/routers/announcementRouter.js
+++ api/src/routers/announcementRouter.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { permission } from "../middleware/permission.js";
 import { getAnnouncements, sendAnnouncement } from "../queries/messageQueries.js";
+import { getAnnouncementsValidation, sendAnnouncementValidation } from "../validation/announcementValidation.js";
+import { validateRequest } from "../middleware/validate.js";
+import { matchedData } from "express-validator";
 
 const announcementRouter = Router();
 
@@ -10,9 +13,10 @@ const announcementRouter = Router();
 // Send announcement
 announcementRouter.post(
 	"/",
+	...validateRequest(sendAnnouncementValidation),
 	...permission("supervisor"),
 	async (request, response) => {
-		const { message } = request.body;
+		const { message } = matchedData(request);
 		
 		const messageId = await sendAnnouncement(request.user.id, message);
 		return response.status(201).send({ messageId });
@@ -23,11 +27,12 @@ announcementRouter.post(
 // Get announcements
 announcementRouter.get(
 	"/",
+	...validateRequest(getAnnouncementsValidation),
 	...permission("in company"),
 	async (request, response) => {
-		const { date, skip, limit } = request.query;
+		const { date, skip, limit } = matchedData(request);
 		
-		const announcements = await getAnnouncements(request.user.companyId, date, Number(skip), Number(limit));
+		const announcements = await getAnnouncements(request.user.companyId, date, skip, limit);
 		return response.send({ announcements });
 	}
 );
